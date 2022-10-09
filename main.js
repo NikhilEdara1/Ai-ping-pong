010aab44e28e56537855b6fadcd4c875d227688c
NikhilEdara1 <96823451+NikhilEdara1@users.noreply.github.com>
2022-10-09 07:09:15 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -22,11 +22,23 @@ var ball = {
 }
 
 function setup(){
-  var canvas =  createCanvas(700,600);
+  var canvas =  createCanvas(700,600);  
+  canvas.parent('canvas');
+  video = createCapture(VIDEO);
+  video.size(700,600);
+  video.hide();
+
+  poseNet =ml5.poseNet(video,modelloaded);
+poseNet.on('pose',posedetection);
+}
+
+function modelloaded(){
+  console.log("modeloaded");
 }
 
 
 function draw(){
+  image(video,0,0,700,600);
 
  background(0); 
 
